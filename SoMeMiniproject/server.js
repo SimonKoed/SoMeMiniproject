5f764a135f9a8a5f53dd644f92b170e55a74a2ac
SimonKoed <31965808+SimonKoed@users.noreply.github.com>
2019-05-14 23:53:13 +0200

    minor stuff done to server

--- SoMeMiniproject/server.js
+++ SoMeMiniproject/server.js
@@ -12,10 +12,6 @@ const app = express();
 app.use(bodyParser.json())
 //app.use(cors());
 
-app.use((req, res, next) => {
-	console.log('<h1>HHHHHEEEEEEELLLOOOO</h1>');
-	next();
-})
 
 const database = {
 	users: [ // array of objects
@@ -46,19 +42,22 @@ const database = {
 }
 
 app.get('/', (req, res) => {
-	//res.send(database.users);
-	res.send('TESTTEST');
+	res.send(database.users);
+	//res.send('TESTTEST');
 })
 
 
 // json (feature for 'response') instead of send has some extra features
+// Think that this thing is actually not nescessary. '/profile/:id' can return the user which i guess is all we really need to do
 app.post('/signin', (req, res) => {
 
-	if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+	database.users.forEach(user => {
+	 	if(req.body.email === database.users.email && req.body.password === database.users.password) {
 		res.json('Success');
 	} else {
 		res.status(400).json('error logging in');
 	}
+	})
 })
 
 app.post('/register', (req, res) => {
@@ -69,7 +68,6 @@ app.post('/register', (req, res) => {
 			name: name,
 			email: email,
 			password: password,
-			entries: 0, // how many times something has been done by the user
 			joined: new Date() // will be used whenever it is run
 		})
 		res.json(database.users[database.users.length-1]); // returns the last member of the users array			
@@ -112,5 +110,6 @@ app.put('/image', (req, res) => {
 app.listen(3001, function() { /* Listens for a server on the port 3000 */
 	console.log("app is running on port 3000");
 });
+
 
 
